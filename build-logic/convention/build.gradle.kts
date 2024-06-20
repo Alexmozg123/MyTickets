b2423e7774577874005c6106dae27c091e3cb772
Alex <alexwot1212@yandex.ru>
2024-06-20 10:57:51 +0300

    init repo commit

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    google()
+}
+
+dependencies {
+    implementation(libs.plugin.android)
+    implementation(libs.plugin.kotlin)
+    implementation(libs.plugin.serialization)
+}
+
+kotlin {
+    sourceSets.getByName("main").kotlin.srcDir("build-logic/src/main/kotlin")
+}
